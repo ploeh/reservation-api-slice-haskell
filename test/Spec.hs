2b4ba89f3bcc8b6c984deb6888afc721e6dfb1c4
Mark Seemann <mark@ploeh.dk>
2019-04-28 19:04:52 +0200

    Add validation module

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,11 +5,13 @@ import Test.Hspec
 import Test.Hspec.Wai
 import Test.Hspec.Wai.JSON
 import API (app)
+import ValidationSpec
 import ReservationAPISpec
 
 main :: IO ()
 main = hspec $ do
   spec
+  validationSpec
   reservationAPISpec
 
 spec :: Spec
---
+++ test/ValidationSpec.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module ValidationSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+import Validation
+
+instance (Arbitrary e, Arbitrary a) => Arbitrary (Validated e a) where
+  arbitrary = Validated <$> arbitrary
+
+validationSpec = do
+  describe "Validated" $ do
+    it "obeys the applicative identity law" $ property $ \
+      (v :: Validated String Int) -> do
+      v `shouldBe` pure id <*> v
+
+    it "obeys the applicative composition law" $ property $ do
+      u :: Validated Ordering (String -> Ordering) <- arbitrary
+      v :: Validated Ordering (Integer -> String) <- arbitrary
+      w :: Validated Ordering Integer <- arbitrary
+
+      let left = pure (.) <*> u <*> v <*> w
+      let right = u <*> (v <*> w)
+
+      return $ left `shouldBe` right
+
+    it "obeys the applicative homomorphism law" $ property $ do
+      f :: String -> Double <- arbitrary
+      x :: String <- arbitrary
+
+      let left :: Validated String Double = pure f <*> pure x
+      let right = pure (f x)
+
+      return $ left `shouldBe` right
+
+    it "obeys the applicative interchange law" $ property $ do
+      u :: Validated [Double] (Integer -> String) <- arbitrary
+      y :: Integer <- arbitrary
+
+      let left = u <*> pure y
+      let right = pure ($ y) <*> u
+
+      return $ left `shouldBe` right
