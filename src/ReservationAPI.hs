beda11ec8c7a570168e14d5c273f7fa86384e9a7
Mark Seemann <mark@ploeh.dk>
2019-05-02 21:13:01 +0200

    Change reservation date to ZonedTime

--- src/ReservationAPI.hs
+++ src/ReservationAPI.hs
@@ -1,17 +1,29 @@
 {-# LANGUAGE DataKinds     #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module ReservationAPI where
 
 import Data.Char
+import Data.Coerce
 import Data.UUID
+import Data.Time.Calendar
+import Data.Time.LocalTime
 import Data.Aeson
 import GHC.Generics
 import Servant
 
+newtype ZonedTimeStructEq =
+  ZonedTimeStructEq ZonedTime
+  deriving (Show, ToJSON, FromJSON)
+
+instance Eq ZonedTimeStructEq where
+  (ZonedTimeStructEq (ZonedTime lt1 tz1)) == (ZonedTimeStructEq (ZonedTime lt2 tz2)) =
+    lt1 == lt2 && tz1 == tz2
+
 data Reservation = Reservation
   { reservationId :: UUID
-  , reservationDate :: String
+  , reservationDate :: ZonedTimeStructEq
   , reservationName :: String
   , reservationEmail :: String
   , reservationQuantity :: Int
@@ -34,7 +46,11 @@ type ReservationAPI =
   :<|> ReqBody '[JSON] Reservation :> Post '[JSON] ()
 
 reservation :: UUID -> Reservation
-reservation rid = Reservation rid "2019-10-04" "Ploeh" "ploeh@example.com" 3
+reservation rid =
+  let rd = coerce $ ZonedTime
+            (LocalTime (fromGregorian 2019 10 4) (TimeOfDay 18 30 0))
+            (hoursToTimeZone 1)
+  in Reservation rid rd "Ploeh" "ploeh@example.com" 3
 
 reservationServer :: Server ReservationAPI
 reservationServer = getReservation :<|> postReservation
--- test/ReservationAPISpec.hs
+++ test/ReservationAPISpec.hs
@@ -2,30 +2,52 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module ReservationAPISpec where
 
+import Control.Monad
+import Data.Coerce
 import Data.UUID
+import Data.Time.Calendar
+import Data.Time.LocalTime
 import Data.Aeson
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Instances.Time
+import Test.QuickCheck.Instances.UUID
 import ReservationAPI
 
+instance Arbitrary ZonedTimeStructEq where
+  arbitrary = ZonedTimeStructEq <$> arbitrary
+
 instance Arbitrary Reservation where
-  arbitrary = do
-    (w1, w2, w3, w4) <- arbitrary
-    let rid = fromWords w1 w2 w3 w4
-    rd <- arbitrary
-    rn <- arbitrary
-    re <- arbitrary
-    rq <- arbitrary
-    return $ Reservation rid rd rn re rq
+  arbitrary =
+    liftM5 Reservation arbitrary arbitrary arbitrary arbitrary arbitrary
+
+-- The time zone flag and names aren't preserved when rendered as JSON
+makeZonedTimeRoundTrippable :: ZonedTimeStructEq -> ZonedTimeStructEq
+makeZonedTimeRoundTrippable (ZonedTimeStructEq (ZonedTime lt tz)) =
+  coerce $ ZonedTime lt $ tz { timeZoneSummerOnly = False, timeZoneName = "" }
 
 reservationAPISpec :: Spec
 reservationAPISpec = do
   describe "Reservation JSON" $ do
     it "renders correctly" $ do
       let rid = fromWords 872411231 2362592316 2161598850 3450687078
-      let json = encode $ Reservation rid "2019-04-12" "Jo" "j@example.com" 3
-      json `shouldBe` "{\"id\":\"33fff05f-8cd2-4c3c-80d7-6182cdad4e66\",\"date\":\"2019-04-12\",\"name\":\"Jo\",\"email\":\"j@example.com\",\"quantity\":3}"
-    it "round-trips" $ property $ \(expected :: Reservation) -> do
+      let rd = coerce $ ZonedTime
+            (LocalTime (fromGregorian 2019 4 12) (TimeOfDay 19 0 0))
+            (hoursToTimeZone 2)
+
+      let json = encode $ Reservation rid rd "Jo" "j@example.com" 3
+
+      json `shouldBe` "{\"id\":\"33fff05f-8cd2-4c3c-80d7-6182cdad4e66\",\
+                       \\"date\":\"2019-04-12T19:00:00+02:00\",\
+                       \\"name\":\"Jo\",\
+                       \\"email\":\"j@example.com\",\
+                       \\"quantity\":3}"
+
+    it "round-trips" $ property $ \(r :: Reservation) -> do
+      let expected =
+            r { reservationDate = makeZonedTimeRoundTrippable $ reservationDate r }
       let json = encode $ expected
+
       let actual = decode json
+
       actual `shouldBe` Just expected
